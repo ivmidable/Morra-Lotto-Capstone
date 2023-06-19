232bb5d32befb56e228e0ddf328b2cafd8f6fd8d
Richard <ivmidable@gmail.com>
2023-06-19 00:13:56 -0400

    add typescript blake3 hashing

--- tests/morra-lotto.ts
+++ tests/morra-lotto.ts
@@ -1,16 +1,47 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { MorraLotto } from "../target/types/morra_lotto";
+import { PublicKey } from "@solana/web3.js";
+//import { MorraLotto } from "../target/types/morra_lotto";
+import { blake3 } from 'hash-wasm';
+import { publicKey } from "@coral-xyz/anchor/dist/cjs/utils";
 
 describe("morra-lotto", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  //anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.MorraLotto as Program<MorraLotto>;
+  //const program = anchor.workspace.MorraLotto as Program<MorraLotto>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    // const tx = await program.methods.initialize().rpc();
+    //console.log("Your transaction signature", tx);
+  });
+
+  it("Create a blake3 hash", async () => {
+    let key = anchor.web3.Keypair.generate();
+    let hash = await createHash(1, 200, key.publicKey);
+    console.log(hash);
+
   });
 });
+
+function createHash(hand: number, guess: number, secret: PublicKey) {
+  const bufArray: Array<Buffer> = [];
+  bufArray.push(writeUInt8(1));
+  bufArray.push(writeUInt16(200));
+  bufArray.push(secret.toBuffer());
+  let buf = Buffer.concat(bufArray);
+  return blake3(buf);
+}
+
+function writeUInt8(number: number) {
+  const buf = Buffer.alloc(1);
+  buf.writeUInt8(number);
+  return buf;
+}
+
+function writeUInt16(number: number) {
+  const buf = Buffer.alloc(2);
+  buf.writeUInt8(number);
+  return buf;
+}
